94209a64373f3118494e23c138f3c059dc4d4cc7
AngelaBello-creator <angelabellomedina@gmail.com>
2025-08-27 14:32:20 +0200

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+
+import './App.css';
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
 
 function App() {
-  
-
   return (
     <>
-      
+      <Header />
+      <main>
+        <Home />
+        
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
