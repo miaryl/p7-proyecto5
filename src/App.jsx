d783a0392341f64df2b2298d818c6dd9225d2a86
Paola <mpulgav@gmail.com>
2025-08-29 12:00:00 +0200

    fix merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
 import Home from "./pages/home/Home.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CardsShuffle from "./pages/pickYourCards/cardsShuffle/CardsShuffle.jsx";
 import { Toaster } from "react-hot-toast"; 
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Toaster position="top-center" />
