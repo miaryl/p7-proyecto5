98ee8b6517804a5b2a47072796f73aa87d66f8dd
Paola <mpulgav@gmail.com>
2025-08-29 11:40:09 +0200

    Add toast alert to HomeForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Home from "./pages/home/Home.jsx";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CardsShuffle from "./pages/pickYourCards/cardsShuffle/CardsShuffle.jsx";
+import { Toaster } from "react-hot-toast"; 
 
 function App() {
   return (
     <BrowserRouter>
+   
+      <Toaster position="top-center" />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shuffle" element={<CardsShuffle />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
-
