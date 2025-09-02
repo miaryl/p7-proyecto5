2c35c4e0f42317f2a30fa663eea8617817b88e46
AngelaBello-creator <angelabellomedina@gmail.com>
2025-09-02 20:39:00 +0200

    finishing header

--- src/components/modals/InfoModal.jsx
+++ src/components/modals/InfoModal.jsx
@@ -7,14 +7,14 @@ export default function InfoModal({ open, onClose }) {
         <strong>Las Venus del Tarot</strong>
   
       <ul>
-        <li> Esta aplicación web en React es un tarot especial donde cada carta boca abajo
+        Esta aplicación web en React es un tarot especial donde cada carta boca abajo
         revela la historia de <strong>una mujer que ha dejado huella en el mundo STEM. </strong>
       
         El usuario puede barajar el mazo, elegir tres cartas y, al girarlas,
         descubrir sus logros junto con un mensaje inspirador.
     
         Cada lectura se puede guardar en un historial personal con nombre y fecha.
-        Desde allí es posible consultar, editar o eliminar lecturas pasadas.</li>
+        Desde allí es posible consultar, editar o eliminar lecturas pasadas.
        </ul>
     </Modal>
   );
