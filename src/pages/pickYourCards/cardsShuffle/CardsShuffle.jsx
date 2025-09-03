3d2c642144c8027638fe623ddf945f26d5243769
Mio <gusw.0246.lpsyg@gmail.com>
2025-09-03 11:10:54 +0200

    removed module from css

--- src/pages/pickYourCards/cardsShuffle/CardsShuffle.jsx
+++ src/pages/pickYourCards/cardsShuffle/CardsShuffle.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./CardsShuffle.module.scss";
+import "./CardsShuffle.scss";
 import toast from "react-hot-toast";
 import { api } from "../../../services/apiCard";
 import RandomCards from "../../../components/randomCards/RandomCards";
