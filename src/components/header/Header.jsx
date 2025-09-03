6946806bb40e6f4baeab480252247b8a35db5fa7
AngelaBello-creator <angelabellomedina@gmail.com>
2025-09-03 10:53:22 +0200

    adding username data to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import s from "./Header.module.scss";
 import instagram from "../../assets/instagram.svg";
@@ -7,43 +8,65 @@ import logo from "../../assets/logo.svg";
 import InfoModal from "../modals/InfoModal";
 
 export default function Header() {
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-  const isInfo = pathname === "/info";
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const isInfo = pathname === "/info";
+  const [userName, setUserName] = useState('');
 
-  const openInfo = () => navigate("/info");
-  const closeInfo = () => navigate(-1);
+  const openInfo = () => navigate("/info");
+  const closeInfo = () => navigate(-1);
 
-  return (
-    <>
-      <header className={s.header}>
-        <div className={s.row}>
-          {/* Izquierda: logo (un pelín más grande) */}
-          <div className={s.left}>
-            <button
-              className={s.iconBtn}
-              onClick={() => navigate("/")}
-              aria-label="Ir al inicio"
-            >
-              <img
-                src={logo}
-                alt="Las Venus del Tarot"
-                className={`${s.icon} ${s.logo}`}   // 👈 cambio
-              />
-            </button>
-          </div>
 
-          {/* Derecha: iconos */}
-          <div className={s.icons}>
-            <img src={instagram} alt="Instagram" className={s.icon} />
-            <button className={s.iconBtn} onClick={openInfo} aria-label="Información">
-              <img src={info} alt="" aria-hidden="true" className={s.icon} />
-            </button>
-          </div>
-        </div>
-      </header>
+  useEffect(() => {
+    const userId = localStorage.getItem('userId');
 
-      <InfoModal open={isInfo} onClose={closeInfo} />
-    </>
-  );
+    if (userId) {
+      fetch(`http://localhost:3000/users/${userId}`)
+        .then(response => response.json())
+        .then(data => {
+          if (data && data.name) {
+            setUserName(data.name);
+          } else {
+            setUserName('Invitado');
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching user data:', error);
+          setUserName('Invitado');
+        });
+    } else {
+      setUserName('Invitado');
+    }
+  }, []); 
+
+  return (
+    <>
+      <header className={s.header}>
+        <div className={s.row}>
+          <div className={s.left}>
+            <button
+              className={s.iconBtn}
+              onClick={() => navigate("/")}
+              aria-label="Ir al inicio"
+            >
+              <img
+                src={logo}
+                alt="Las Venus del Tarot"
+                className={`${s.icon} ${s.logo}`}
+              />
+            </button>
+          </div>
+          <span className={s.userName}>{userName}</span>
+          <div className={s.icons}>
+            <img src={instagram} alt="Instagram" className={s.icon} />
+            <button className={s.iconBtn} onClick={openInfo} aria-label="Información">
+              <img src={info} alt="" aria-hidden="true" className={s.icon} />
+            </button>
+          </div>
+        </div>
+      </header>
+
+      <InfoModal open={isInfo} onClose={closeInfo} />
+    </>
+  );
 }
